0e869af2b1ed3ddc8531e65e44b7ff609c849272
irohanrajput <itusharsingh27@gmail.com>
2023-08-24 11:55:14 +0530

    practice

--- random_codes/person_details.py
+++ random_codes/person_details.py
@@ -4,14 +4,6 @@ class person:
         self.home = city
         self.job = occupation
         self.yearofbirth = yob
-
-        
-class person:
-    def __init__(self, name, city, occupation, yob):
-        self.name = name
-        self.home = city
-        self.job = occupation
-        self.yearofbirth = yob
     def speakNow(self):
         print(f'My name is {self.name}, I am from {self.home}. For living, I work as {self.job} and I was born in year {self.yearofbirth}')
 
